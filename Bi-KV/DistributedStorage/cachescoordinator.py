83a1d29c17041c69adf99c3cf3921fa265c97bb0
shudorcl <1985366171@qq.com>
2024-12-20 14:26:42 +0800

    perf(CacheCoordinator): remove old process_requests functions

--- Bi-KV/DistributedStorage/cachescoordinator.py
+++ Bi-KV/DistributedStorage/cachescoordinator.py
@@ -42,42 +42,6 @@ class CacheCoordinator:
 
     def process_requests(self):
         print("[CacheCoordinator] 开始处理请求")
-        while not self.request_table.empty():
-            executable_requests = []
-            unexecutable_requests = []
-            while not self.request_table.empty():
-                req = self.request_table.get_nowait()
-                send_worker, recv_worker, executing = req['send_worker'], req['recv_worker'], req['executing']
-                if not executing and self.cpu_state_table[send_worker]['status'] == 'idle' and self.cpu_state_table[recv_worker]['status'] == 'idle':
-                    with self.lock:
-                        self.cpu_state_table[send_worker]['status'] = 'sending'
-                        self.cpu_state_table[recv_worker]['status'] = 'receiving'
-                        req['executing'] = True
-                    executable_requests.append(req)
-                else:
-                    # 无法执行则加入无法执行list，后续重新入队
-                    unexecutable_requests.append(req)
-            for req in unexecutable_requests:
-                self.request_table.put_nowait(req)
-            if not executable_requests:
-                time.sleep(0.1)
-                continue
-
-            executable_requests.sort(key=lambda x: x['request_id'])
-            threads = []
-            for req in executable_requests:
-                # print(f"[CacheCoordinator]About to execute {req}")
-                thread = Thread(target=self._execute_request, args=(req,))
-                threads.append(thread)
-                thread.start()
-
-            for thread in threads:
-                thread.join()
-
-        print("[CacheCoordinator] 所有请求处理完成")
-
-    def process_request_new(self):
-        print("[CacheCoordinator] 开始处理请求")
         idle_time_counter = 0
         has_excuted = False
         while self.process_flag:
@@ -127,38 +91,6 @@ class CacheCoordinator:
 
         print("[CacheCoordinator] 所有请求处理完成")
 
-    def process_requests_old(self):
-        print("[CacheCoordinator] 开始处理请求")
-        while self.request_table:
-            executable_requests = []
-            for request_id, req in list(self.request_table.items()):
-                send_cpu, recv_cpu, executing = req['send_cpu'], req['recv_cpu'], req['executing']
-                if not executing and self.cpu_state_table[send_cpu]['status'] == 'idle' and self.cpu_state_table[recv_cpu]['status'] == 'idle':
-                    with self.lock:
-                        self.cpu_state_table[send_cpu]['status'] = 'sending'
-                        self.cpu_state_table[recv_cpu]['status'] = 'receiving'
-                        self.request_table[request_id]['executing'] = True
-                    executable_requests.append(request_id)
-
-            if not executable_requests:
-                time.sleep(0.1)
-                continue
-
-            executable_requests.sort()
-            threads = []
-            for request_id in executable_requests:
-                req = self.request_table[request_id]
-                send_cpu, recv_cpu = req['send_cpu'], req['recv_cpu']
-                thread = Thread(target=self._execute_request, args=(request_id, send_cpu, recv_cpu))
-                threads.append(thread)
-                thread.start()
-
-            for thread in threads:
-                thread.join()
-
-        print("[CacheCoordinator] 所有请求处理完成")
-        return
-
     def _execute_request(self, req:Dict):
         request_id, send_worker, recv_worker = req['request_id'], req['send_worker'], req['recv_worker']
         self.finished_table[request_id] = False
@@ -202,7 +134,7 @@ class CacheCoordinator:
         self.worker_ref = workers_rref
         print(f"[CacheCoordinator] 已设置workers rref信息 长度为{len(self.worker_ref)}")
         self.process_flag = True
-        self.process_request_new()
+        self.process_requests()
         # send_worker_ref = self.worker_ref[0]
         # owner_worker_ref = send_worker_ref.owner()  
         # from Worker.Worker import Worker
